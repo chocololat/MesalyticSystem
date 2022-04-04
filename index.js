2a9bdc5bd9a0e98a09e4e3003ef46d6086921dff
MesaVirep <atamatori56@gmail.com>
2022-04-04 23:18:21 +0200

    feat: welcome message

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { Client, Intents, MessageActionRow, MessageButton } = require('discord.js');
-const { token, componentsID } = require('./config.json');
+const { token, componentsID, events } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', async () => {
 	console.log('Ready!');
 });
+
+client.on("guildMemberAdd", member => {
+    let channel = client.guilds.cache.first().channels.cache.get(events.guildMemberAdd.channelID);
+
+    channel.send({ content: events.guildMemberAdd.content.replace("%MEMBERID%", member.id) })
+})
 
 client.on('interactionCreate', async interaction => {
     if (interaction.customId === componentsID.rules.id) {
